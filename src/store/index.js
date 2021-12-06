800794cfb4f4c4ae737f4e8086e67ccb357375c1
闫威 <779463900@qq.com>
2021-12-06 14:58:23 +0800

    finish

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,83 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from 'axios'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+  actions: {
+    getList(context) {
+      axios.get('/list.json').then(({ data }) => {
+        context.commit('initList', data)
+      })
+    }
   },
   mutations: {
+    initList(state, list) {
+      state.list = list
+    },
+    // 为inputValue赋值
+    setInputValue(state, val) {
+      state.inputValue = val
+    },
+    // 添加列表项
+    addItem(state) {
+      const obj = {
+        id: state.nextId,
+        info: state.inputValue.trim(),
+        done: false
+      }
+      state.list.push(obj)
+      state.nextId++
+      state.inputValue = ''
+    },
+    // 删除
+    removeItem(state, id) {
+      // 根据id查找对应的索引
+      const i = state.list.findIndex(x => x.id === id)
+      // 根据索引删除
+      if (i !== -1) {
+        state.list.splice(i, 1)
+      }
+    },
+    // 修改复选框
+    changeStatus(state, param) {
+      const i = state.list.findIndex(x => x.id === param.id)
+      if (i !== -1) {
+        state.list[i].done = param.status
+      }
+    },
+    // 清除已完成的任务
+    cleanDone(state) {
+      state.list = state.list.filter(x => x.done === false)
+    },
+    changeViewKey(state, key) {
+      state.viewKey = key
+    }
   },
-  actions: {
+  getters: {
+    // 未完成数
+    unDoneLength(state) {
+      return state.list.filter(x => x.done === false).length
+    },
+    infolist(state) {
+      if (state.viewKey === 'all') {
+        return state.list
+      }
+      if (state.viewKey === 'undone') {
+        return state.list.filter(x => !x.done)
+      }
+      if (state.viewKey === 'done') {
+        return state.list.filter(x => x.done)
+      }
+      return state.list
+    }
+  },
+  state: {
+    list: [],
+    // 文本框内容
+    inputValue: '',
+    nextId: 5,
+    viewKey: 'all'
   },
   modules: {
   }
